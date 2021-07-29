9820f4d36ae65e509f773d45d3ce7d6793c79445
Philippe Arteau <philippe.arteau@gmail.com>
2021-07-29 04:29:22 -0400

    Different report file being upload.. just to see

---
+++ src/main/java/com/h3xstream/sandbox/SpecialCase3.java
@@ -0,0 +1,20 @@
+package com.h3xstream.sandbox;
+
+import java.util.Random;
+
+public class SpecialCase3 {
+
+
+}
+
+class CanYouSeeMeOuter2 {
+
+    class FollowTheWhiteRabbit {
+        public String generateToken(String[] args){
+
+            int minorEdit = 10+25;
+
+            return ""+new Random().nextLong(); //FINDME: Insecure random generator
+        }
+    }
+}
